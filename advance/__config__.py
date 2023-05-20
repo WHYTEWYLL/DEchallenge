3bb3d318563a074114c819ea33ac598b4bc06c76
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2023-05-21 01:33:17 +0200

    fix: temaplte

--- advance/__config__.py
+++ advance/__config__.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ALCHEMY_TOKEN = os.getenv("privateKeyToAccount") or os.getenv(
-    "INPUT_privateKeyToAccount"
-)
+ALCHEMY_TOKEN = os.getenv("privateKeyToAccount") or os.getenv("ALCHEMY_TOKEN")
 
-
+print("THIS IS ALCHEMY_TOKEN")
+print(ALCHEMY_TOKEN)
 BASE_URL = "eth-mainnet.g.alchemy.com/v2/"
 
 CONTRACT_ADDRESS = "0xbaac2b4491727d78d2b78815144570b9f2fe8899"
